7e893e7bf34ff248dd79612aa75718dd33d176f8
Jarek Bigaj <j.bigaj@interia.pl>
2023-03-25 19:26:47 +0100

    connect to external weather api

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,18 +1,91 @@
 import styled from "styled-components";
 import { useState } from "react";
+import { informationAboutWeather, informationAboutWeatherInSelectedCity } from "./connection";
 
+const citiesButtonsValues = ['Krakow', 'Marcowka', 'Warsaw'];
 
 function App() {
-  const value = "wow"
+  const [currentCity, setCurrenCity] = useState(() => {
+    return 'Krakow'
+  });
+
+  const informationCurrentCityWeather = informationAboutWeather((currentCity));
+
+  const showCurrentCityWeather = fetch(informationCurrentCityWeather)
+  .then(response => response.json())
+  .then(response => {
+    const {current_weather} = response;
+    console.log(current_weather);
+  })
+
+
   return (
     <div>
-      <Button value={value}/>
+      <WeatherAppWrapper>
+        <Date/>
+        <Time/>
+        <Title/>
+        <ButtonBox>
+          {
+            citiesButtonsValues.map((value,index) => {
+              return <Button key={index} value={value} />
+            })
+          }
+        </ButtonBox>
+        <Temperature/>
+        <Wind/>
+        <InformationAboutWheater/>
+      </WeatherAppWrapper>
     </div>
   );
 }
 
-const Button = (value) =>{
+const WeatherAppWrapper = ({children}) => {
+  return <div>{children}</div>
+}
+
+const Date = () =>{
+  return <span>data</span>
+}
+
+const Time = () =>{
+  return <span>Time</span>
+}
+
+const Title = () =>{
+  return <h1>Pogodynka</h1>
+}
+
+const ButtonBox = ({children}) =>{
+  return <div>{children}</div>
+}
+
+const Button = ({value}) =>{
   return <button>{value}</button>
 }
 
+const Temperature = () => {
+  return <span>Temperature</span>
+}
+
+const Wind = () => {
+  return (
+    <div>
+      <WindDirection/>
+      <WindSpeed/>
+    </div>
+  )
+}
+
+const WindDirection = () => {
+  return <span>WindDirection</span>
+}
+
+const WindSpeed = () => {
+  return <span>WindSpeed</span>
+}
+
+const InformationAboutWheater = () => {
+  return <span>InformationAboutWheater</span>
+}
 export default App;
--- weather-app/src/connection.js
+++ weather-app/src/connection.js
@@ -1,3 +1,10 @@
 const API_URL_KRAKOW = 'https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&current_weather=true';
 const API_URL_WARSAW ='https://api.open-meteo.com/v1/forecast?latitude=52.23&longitude=21.01&current_weather=true';
 const API_URL_MARCOWKA ='https://api.open-meteo.com/v1/forecast?latitude=49.79&longitude=19.62&current_weather=true';
+
+export const informationAboutWeather = (selectedCity) => {
+    switch(selectedCity){
+        case `Krakow` : 
+        return API_URL_KRAKOW;
+    }
+}
